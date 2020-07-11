c37a3f958364a8dab83817e4510606b5a15b92bd
Titus von der Malsburg <malsburg@posteo.de>
2020-07-11 11:19:16 +0200

    Various minor fixes

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 
 var chatlink = "https://www.duckduckgo.com"
 
-var root = document.body
-
 var Navigation = {
     view: function() {
         var x = function(route) {if (route == m.route.get()) {return "nav-link active"} else {return "nav-link"}}
@@ -21,7 +19,7 @@ var Navigation = {
                    m("a", {class: x("/poster_session_3"), href: "#!/poster_session_3"}, "Posters day 3 (Sept 5)")])]),
             // m("li", {class: "nav-item"}, m("a", {class: x("/authors"), href: "#!/authors"}, "Authors")),
         ]
-        return m("nav", {class: "navbar navbar-expand-lg navbar-light bg-light"},
+        return m("nav", {class: "navbar navbar-expand-lg navbar-dark bg-dark"},
                  [
                      m("div", {class: "navbar-brand"}, m("a", {href: "#", class: "navbar-brand"}, "AMLaP 2020 Programme")),
                      m.trust("<button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarSupportedContent\" aria-controls=\"navbarSupportedContent\" aria-expanded=\"false\" aria-label=\"Toggle navigation\"> <span class=\"navbar-toggler-icon\"></span> </button>"),
@@ -68,8 +66,10 @@ function SessionFactory(s) {
                 session = m("a", {href: "#!/poster_session_2"}, session)
             } else if (session == "Poster session 3") {
                 session = m("a", {href: "#!/poster_session_3"}, session)
-            } else if (session.match(/– .+ break –/) || session.match(/Social chat/)) {
+            } else if (session.match(/Social chat/)) {
                 session = m("a", {href: chatlink}, session)
+            } else if (session.match(/– .+ break –/) || session.match(/Social chat/)) {
+                session = m("center", m("a", {href: chatlink}, session))
             }
             if (s.id == null)
                 session = m("strong", {class: "lead", style: "font-weight: bold"}, session)
@@ -90,13 +90,18 @@ function SessionsFactory(day, date) {
                     l.push(m(SessionFactory(s)))
             }
             return [m(Navigation),
-                    m("main", {class: "container"}, [
-                        m("h1", {class: "display-4"}, "Sessions Day " + day),
-                        m("p", {class: "lead"}, date)]),
-                    m("table", {class: "table table-sm table-striped"}, [
-                        m(SessionsTableHeader),
-                        m("tbody", l),
-                    ]),
+                    m("main", {class: "container", id: "main"}, [
+                        m("div", {class: "container mb-3"}, [
+                            m("h1", {class: "display-4"}, "Sessions Day " + day),
+                            m("p", {class: "lead"}, date),
+                            m("a", {class: "btn btn-primary btn-sm mr-1", href: "https://zoom.us", target: "_blank"}, "Join on Zoom"),
+                            m("a", {class: "btn btn-primary btn-sm mr-1", href: "https://twitch.tv", target: "_blank"}, "Watch on Twitch"),
+                        ]),
+                        m("table", {class: "table table-sm table-striped"}, [
+                            m(SessionsTableHeader),
+                            m("tbody", l),
+                        ]),
+                    ])
                    ]
         }
     }
@@ -125,15 +130,16 @@ function PosterFactory(id, authors, title, links) {
             a = a.flat()
             a.pop()
             return m("tr", [
-                m("td", m("a", {class: "lead", href: id+".pdf"}, "#" + id)),
+                // m("td", m("a", {class: "lead", href: id+".pdf"}, "#" + id)),
+                m("td", {class: "lead"}, "#" + id),
                 m("td", [
                     m("a", {class: "lead", href: id+".pdf"}, title),
                     m("br"),
                     a,
                     m("br"),
-                    m("a", {class: "btn btn-primary btn-sm py-0 mr-1", href: id + ".pdf"}, "abstract"),
-                    m("a", {class: "btn btn-primary btn-sm py-0 mr-1", href: id + "_poster.pdf"}, "poster"),
-                    m("a", {class: "btn btn-primary btn-sm py-0 mr-1", href: "https://meet.jit.si/AMLaP2020_poster_" + id}, "video chat"),
+                    m("a", {class: "btn btn-primary btn-sm py-0 mr-1", href: id + ".pdf"}, "Abstract"),
+                    m("a", {class: "btn btn-primary btn-sm py-0 mr-1", href: id + "_poster.pdf"}, "Poster"),
+                    m("a", {class: "btn btn-primary btn-sm py-0 mr-1", href: "https://meet.jit.si/AMLaP2020_poster_" + id}, "Video chat"),
                 ])
             ])
         }
@@ -149,18 +155,19 @@ function PosterSessionFactory(number, date) {
                     l.push(m(PosterFactory(p.id, p.authors, p.title)))
             }
             return [m(Navigation),
-                    m("main", {class: "container"}, [
-                        m("h1", {class: "display-4"}, "Poster session " + number),
-                        m("p", {class: "lead"}, date)]),
+                    m("main", {class: "container", id: "main"}, [
+                        m("div", {class: "container"}, [
+                          m("h1", {class: "display-4"}, "Poster session " + number),
+                          m("p", {class: "lead"}, date)]),
                     m("table", {class: "table table-sm table-striped"}, [m(PosterTableHeader), m("tbody", l)])
-                   ]
+                    ])]
         }
     }
 }
 
-var PosterSession1 = PosterSessionFactory(1, "Thursday, 3 September 2020, 16:00–17:30 (UTC+2)")
-var PosterSession2 = PosterSessionFactory(2, "Friday, 4 September 2020, 14:00–15:30 (UTC+2)")
-var PosterSession3 = PosterSessionFactory(3, "Saturday, 5 September 2020, 10:00–11:30 (UTC+2)")
+var PosterSession1 = PosterSessionFactory(1, "Thursday, 3 September 2020, 16:00–17:30 (UTC+2)")
+var PosterSession2 = PosterSessionFactory(2, "Friday, 4 September 2020, 14:00–15:30 (UTC+2)")
+var PosterSession3 = PosterSessionFactory(3, "Saturday, 5 September 2020, 10:00–11:30 (UTC+2)")
 
 var Authors = {
     view: function() {
@@ -170,7 +177,7 @@ var Authors = {
     }
 }
 
-m.route(root, "/overview", {
+m.route(document.body, "/overview", {
     "/overview": Overview,
     "/sessions1": Sessions1,
     "/sessions2": Sessions2,
@@ -180,3 +187,4 @@ m.route(root, "/overview", {
     "/poster_session_3": PosterSession3,
     "/authors": Authors,
 })
+
